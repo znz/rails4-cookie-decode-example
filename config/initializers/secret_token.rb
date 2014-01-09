612f5e39566afce03450b4683e60935b0d3d8de1
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-01-09 22:00:34 +0900

    `rails new rails4-cookie-decode-example -B -O -J -T`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4CookieDecodeExample::Application.config.secret_key_base = 'bfc092cf2b5ec9855350f4f0fe716ee62a75cefe65f9d069b62c1ad2de0cd7f0134335843b411c9b2b23e9f759f39f135981def09e5ab8ca632d76c76af717a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4CookieDecodeExample::Application.config.session_store :cookie_store, key: '_rails4-cookie-decode-example_session'
